16eef355ae1d40fe9ff65cbde5276c10e764328d
gask <andry.95.1995@gmail.com>
2021-06-23 11:39:46 +0200

    fixed colors function

--- C++/colors.h
+++ C++/colors.h
@@ -4,11 +4,10 @@
 #include <sstream>
 #include <string>
 
-using namespace std;
 
 template <class T>
-string colors(T s, int n, int n2=0){
-        stringstream ss;
+std::string colors(T s, int n, int n2=0){
+        std::stringstream ss;
         ss<< "\e["<<n2<<";3"<<n<<"m"<<" "<<s<<" "<<"\e[0m";
         return ss.str();
 }
